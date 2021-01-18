2b6d3d701100eacb64a9a12d180d1ee9856990d1
Mihaly Lestak <ws.eclipse@gmail.com>
2021-01-18 15:22:08 +0100

    project-start

---
+++ src/main/java/challenger/mink/MinkApplication.java
@@ -0,0 +1,14 @@
+package challenger.mink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MinkApplication.class, args);
+    System.out.println("what does the mink say?");
+  }
+
+}
---
+++ src/test/java/challenger/mink/MinkApplicationTests.java
@@ -0,0 +1,13 @@
+package challenger.mink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
