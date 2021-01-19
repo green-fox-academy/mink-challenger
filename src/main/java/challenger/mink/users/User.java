0abb88b34e207ed6756ef28fcef7e1aeec46ab80
Rita Csullag <csullagrita@gmail.com>
2021-01-19 16:26:46 +0100

    mokasritus-1 Implement datamigration, modify README with the environment variables

--- src/main/java/challenger/mink/users/User.java
+++ src/main/java/challenger/mink/users/User.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name="users")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
