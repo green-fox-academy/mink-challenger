2e74b22a22818c1d04b589dee003013152a54edb
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2021-01-21 16:57:42 +0100

    Aniko4 - login fixed

--- src/main/java/challenger/mink/security/WebSecurityConfig.java
+++ src/main/java/challenger/mink/security/WebSecurityConfig.java
@@ -5,13 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -45,13 +43,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
         .authorizeRequests()
-        .antMatchers("/", "/login", "/register", "/nosafe")
+        .antMatchers("/", "/login", "/register")
         .permitAll()
         .anyRequest()
         .authenticated()
         .and()
         .formLogin()
         .permitAll()
+        .defaultSuccessUrl("/commitment")
         .and()
         .logout()
         .permitAll();
