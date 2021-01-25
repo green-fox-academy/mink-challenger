be7b7098d50267dfa319d50cdbcbe218370cd69a
Rita Csullag <csullagrita@gmail.com>
2021-01-25 22:03:26 +0100

    mokasritus-6 Create database V1 again

--- src/main/java/challenger/mink/commitments/Commitment.java
+++ src/main/java/challenger/mink/commitments/Commitment.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
@@ -28,8 +29,10 @@ public class Commitment {
   private LocalDate date;
   private boolean done;
   @ManyToOne
+  @JoinColumn(name = "user_id")
   private User user;
   @ManyToOne
+  @JoinColumn(name = "challenge_id")
   private Challenge challenge;
 
   public Commitment(String description, LocalDate date, User user,
--- src/main/resources/db/migration/V1__create_database.sql
+++ src/main/resources/db/migration/V1__create_database.sql
@@ -1,15 +1,29 @@
 CREATE TABLE users
 (
-    id           BIGINT AUTO_INCREMENT,
-    username     VARCHAR(50) NOT NULL UNIQUE,
-    challenge_id BIGINT,
+    id        BIGINT AUTO_INCREMENT,
+    username  VARCHAR(50)  NOT NULL UNIQUE,
+    password  VARCHAR(60)  NOT NULL,
+    email     VARCHAR(320) NOT NULL,
+    user_type VARCHAR(50),
     PRIMARY KEY (id)
-    #FOREIGN KEY (challenge_id) REFERENCES challenges (id)
 );
 
-INSERT INTO users(username, challenge_id)
-VALUES ('Bela', 2),
-       ('Gazsi', 1);
+INSERT INTO users(username, password, email, user_type)
+VALUES ('Bela', '$2y$12$jnlHdPuXJ04ExbPEWxDMjuURSrzurWnWCsbf/RSxxWxLEuYJzuttW', 'gazsi@email.hu', 'ADMIN'),
+       ('Gazsi', '$2y$12$jnlHdPuXJ04ExbPEWxDMjuURSrzurWnWCsbf/RSxxWxLEuYJzuttW', 'bela@email.hu', 'USER');
+
+CREATE TABLE challenges
+(
+    id            BIGINT AUTO_INCREMENT,
+    name          VARCHAR(50) NOT NULL UNIQUE,
+    start_date    DATE,
+    end_date      DATE,
+    minimum_commitment BIGINT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO challenges (name, start_date, end_date, minimum_commitment)
+VALUES ('running challenge', '2021-01-01', '2021-01-31', 1);
 
 CREATE TABLE commitments
 (
@@ -19,27 +33,15 @@ CREATE TABLE commitments
     done         boolean,
     user_id      BIGINT       NOT NULL,
     challenge_id BIGINT       NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (challenge_id) REFERENCES challenges (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 INSERT INTO commitments(description, date, done, user_id, challenge_id)
 VALUES ('One Hour run', '2021-01-19', false, 1, 1),
        ('One Hour run', '2021-01-01', true, 2, 1);
 
-CREATE TABLE challenges
-(
-    id            BIGINT AUTO_INCREMENT,
-    name          VARCHAR(50) NOT NULL UNIQUE,
-    start_date    DATE,
-    end_date      DATE,
-    commitment_id BIGINT,
-    user_id       BIGINT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (commitment_id) REFERENCES commitments (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
 
-INSERT INTO challenges (name, start_date, end_date, commitment_id, user_id)
-VALUES ('running challenge', '2021-01-01', '2021-01-31', 1, 2);
 
 
 
