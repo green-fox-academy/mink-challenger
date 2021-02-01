4d2bb7824a6946ed53eaf8882f45a0b32bed3cdd
Mihaly Lestak <ws.eclipse@gmail.com>
2021-02-01 15:35:22 +0100

    stack-trace-handled

--- src/main/java/challenger/mink/MinkApplication.java
+++ src/main/java/challenger/mink/MinkApplication.java
@@ -2,11 +2,10 @@ package challenger.mink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
 @EnableSwagger2
+@SpringBootApplication
 public class MinkApplication {
 
   public static void main(String[] args) {
